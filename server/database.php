3707a26af4df347bedac68aae9412972a9cbe879
Skripted <palic@gmx.at>
2013-02-02 04:03:18 +0100

    update database.php

--- server/database.php
+++ server/database.php
@@ -3,14 +3,13 @@
 // file updated by skripted - 02.02.13
 require_once('config.php');
 
-function db_connect()
-{	
-    try{
-		return new PDO('mysql:host=' . DBHOST . ';dbname=' . DBNAME, DBUSER, DBPASS);
-	} 
-	catch(PDOException $e) {
-		return "Could not Connect to Server!";
-	}
+function db_connect() {	
+  try {
+    return new PDO('mysql:host=' . DBHOST . ';dbname=' . DBNAME, DBUSER, DBPASS);
+  } 
+  catch(PDOException $e) {
+    return "Could not Connect to Server!";
+  }
 }
 
 ?>
